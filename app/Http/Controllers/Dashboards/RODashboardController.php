bf4af1631b270daa0ae3da410cb3b9e9a28d598f
Splerge <damnsplurge@gmail.com>
2018-08-18 18:19:15 +1000

    yeet

---
+++ app/Http/Controllers/Dashboards/RODashboardController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Dashboards;
+
+use Illuminate\Http\Request;
+
+class RODashboardController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Dashboards/UserDashboardController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Dashboards;
+
+use Illuminate\Http\Request;
+
+class UserDashboardController extends Controller
+{
+	
+}
