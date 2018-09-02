cdaeb9792961824264ceffd5fa0d907fe5d7ddfc
s3511842 <s3511842@student.rmit.edu.au>
2018-09-02 18:18:35 +1000

    Admin Display Users
    Admin Dashboard page to display all users. Displays their ID, Name, Email & Type. Editing functionality to be worked on.

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Input;
+
+class UsersController extends Controller
+{
+    
+    private function checkAuth() {
+    if(\Auth::check() && !(\Auth::user()->isAdmin()) ) {
+        return true;
+    }
+    return false;
+    }
+    
+    public function index()
+    {
+        if ($this->checkAuth()) {
+            return redirect('/home');
+        }
+        
+        $users = User::All();
+        return View::make('admin.users.index')->with('users', $users);
+    }
+}
