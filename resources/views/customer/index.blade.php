c1dd25cd8fb8f091635ba33782caffe769315ac4
Aravind Rajen <s3576105@student.rmit.edu.au>
2018-09-05 23:03:28 +1000

    some radio buttons, still needs work

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -9,16 +9,28 @@
                 <div class="form-group">
                     {{ Form::label('dietary_mode', 'Dietary Mode') }}
                     {{ Form::text('dietary_mode', null, array('class' => 'form-control')) }}
+                    {{ Form::radio('Vegan', '1') }}
+                    {{ Form::radio('Vegetarian', '2') }}
+                    {{ Form::radio('Meat Lover', '3') }}
+                    {{ Form::radio('Halal', '4') }}
+                    {{ Form::radio('Seafood', '5') }}
                 </div>
 
                 <div class="form-group">
+                  <!-- hello -->
                     {{ Form::label('preferred_price_range', 'Preferred Price Range') }}
                     {{ Form::text('preferred_price_range', null, array('class' => 'form-control')) }}
+                    {{ Form::radio('$', '1') }}
+                    {{ Form::radio('$$', '2') }}
+                    {{ Form::radio('$$$', '3') }}
                 </div>
 
                 <div class="form-group">
                     {{ Form::label('preferred_radius_size', 'Preferred Radius Size') }}
                     {{ Form::text('preferred_radius_size', null, array('class' => 'form-control')) }}
+                    {{ Form::radio('Walking distance', '1') }}
+                    {{ Form::radio('Short ride', '2') }}
+                    {{ Form::radio('Comfortable drive', '3') }}
                 </div>
 
                 {{ Form::submit('Edit the Preference!', array('class' => 'btn btn-primary')) }}
