77af19192ca32d31c892527689f3d7c8803000ac
Aravind Rajen <s3576105@student.rmit.edu.au>
2018-09-06 00:03:39 +1000

    some changes

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -9,28 +9,28 @@
                 <div class="form-group">
                     {{ Form::label('dietary_mode', 'Dietary Mode') }}
                     {{ Form::text('dietary_mode', null, array('class' => 'form-control')) }}
-                    {{ Form::radio('Vegan', '1') }}
-                    {{ Form::radio('Vegetarian', '2') }}
-                    {{ Form::radio('Meat Lover', '3') }}
-                    {{ Form::radio('Halal', '4') }}
-                    {{ Form::radio('Seafood', '5') }}
+                    {{ Form::radio('Diet', 'Vegan', '1') }}
+                    {{ Form::radio('Diet', 'Vegetarian', '2') }}
+                    {{ Form::radio('Diet', 'Meat Lover', '3') }}
+                    {{ Form::radio('Diet', 'Halal', '4') }}
+                    {{ Form::radio('Diet', 'Seafood', '5') }}
                 </div>
 
                 <div class="form-group">
                   <!-- hello -->
                     {{ Form::label('preferred_price_range', 'Preferred Price Range') }}
                     {{ Form::text('preferred_price_range', null, array('class' => 'form-control')) }}
-                    {{ Form::radio('$', '1') }}
-                    {{ Form::radio('$$', '2') }}
-                    {{ Form::radio('$$$', '3') }}
+                    {{ Form::radio('Price','$', '1') }}
+                    {{ Form::radio('Price','$$', '2') }}
+                    {{ Form::radio('Price','$$$', '3') }}
                 </div>
 
                 <div class="form-group">
                     {{ Form::label('preferred_radius_size', 'Preferred Radius Size') }}
                     {{ Form::text('preferred_radius_size', null, array('class' => 'form-control')) }}
-                    {{ Form::radio('Walking distance', '1') }}
-                    {{ Form::radio('Short ride', '2') }}
-                    {{ Form::radio('Comfortable drive', '3') }}
+                    {{ Form::radio('Radius','Walking distance', '1') }}
+                    {{ Form::radio('Radius','Short ride', '2') }}
+                    {{ Form::radio('Radius','Comfortable drive', '3') }}
                 </div>
 
                 {{ Form::submit('Edit the Preference!', array('class' => 'btn btn-primary')) }}
