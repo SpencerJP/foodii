fc15f9433457af5a37c475401ddc93e3a4f9e35d
Aravind Rajen <s3576105@student.rmit.edu.au>
2018-09-16 18:18:35 +1000

    Added header and modified the preferences

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -4,7 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="card-body">
-            {{ Form::model($preferences, array('url' =>  route("preferences.update", 0), 'method' => 'PUT')) }}
+            
+                <section class="jumbotron text-center">
+	                <div class="container">
+	                    <h1 class="jumbotron-heading">User preferences</h1>
+	  
+	                    <p class="lead text-muted">Here you can view and edit your preferences based on multiple factors.</p>
+	                </div>
+	            </section>
+
+                {{ Form::model($preferences, array('url' =>  route("preferences.update", 0), 'method' => 'PUT')) }}
 
                 <div class="form-group">
                     {{ Form::label('dietary_mode', 'Dietary Mode') }}
@@ -13,16 +22,19 @@
                     <br/>
                     {{ Form::radio('Diet', '2') }} Vegetarian
                     <br/>
-                    {{ Form::radio('Diet','3') }} Meat Lover
-                    <br/>
                     {{ Form::radio('Diet','4') }} Halal
                     <br/>
-                    {{ Form::radio('Diet','5') }} Seafood
+                </div>
+
+                <div class="form-group">
+                    {{ Form::label('allergies', 'Allergies') }}
                     <br/>
-                    {{ Form::radio('Diet','6') }} Gluten-free
+                    {{ Form::radio('allergies', '1') }} Lactose
                     <br/>
-                    {{ Form::radio('Diet', '7') }} Lactose intolerant
-                    
+                    {{ Form::radio('allergies', '2') }} Gluten
+                    <br/>
+                    {{ Form::radio('alergies','3') }} Nuts
+                    <br/>
                 </div>
 
                 <div class="form-group">
@@ -39,11 +51,11 @@
                 <div class="form-group">
                     {{ Form::label('preferred_radius_size', 'Preferred Radius Size') }}
                     <br/>
-                    {{ Form::radio('Distance', '1') }} Walking Distance
+                    {{ Form::radio('Distance', '1') }} less than 5km
                     <br/>
-                    {{ Form::radio('Distance', '2') }} Short ride
+                    {{ Form::radio('Distance', '2') }} less than 10km
                     <br/>
-                    {{ Form::radio('Distance','3') }} Comfortable drive
+                    {{ Form::radio('Distance','3') }} Stores that deliver
                     <br/>
                 </div>
 
