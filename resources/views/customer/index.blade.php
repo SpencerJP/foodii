1eccbb1e15c05f9b9eae3682109667128d17ee46
Aravind Rajen <s3576105@student.rmit.edu.au>
2018-09-17 01:06:24 +1000

    Done but needs minor improvements

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="card-body">
             
                 <section class="jumbotron text-center">
 	                <div class="container">
-	                    <h1 class="jumbotron-heading">User preferences</h1>
+	                    <h1 class="jumbotron-heading">Dashboard</h1>
 	  
 	                    <p class="lead text-muted">Here you can view and edit your preferences based on multiple factors.</p>
 	                </div>
-	            </section>
+	            </section>  
+
+                <div class="text-centre">
+                <p class="pref">Select your preferences for a more personalised result:</p>
+                </div>
 
                 {{ Form::model($preferences, array('url' =>  route("preferences.update", 0), 'method' => 'PUT')) }}
 
@@ -22,7 +26,7 @@
                     <br/>
                     {{ Form::radio('Diet', '2') }} Vegetarian
                     <br/>
-                    {{ Form::radio('Diet','4') }} Halal
+                    {{ Form::radio('Diet','3') }} Halal
                     <br/>
                 </div>
 
@@ -40,22 +44,22 @@
                 <div class="form-group">
                     {{ Form::label('preferred_price_range', 'Preferred Price Range') }}
                     <br/>
-                    {{ Form::radio('Price', '1') }} $
+                    {{ Form::radio('price', '1') }} $
                     <br/>
-                    {{ Form::radio('Price', '2') }} $$
+                    {{ Form::radio('price', '2') }} $$
                     <br/>
-                    {{ Form::radio('Price','3') }} $$$
+                    {{ Form::radio('price','3') }} $$$
                     <br/>
                 </div>
 
                 <div class="form-group">
                     {{ Form::label('preferred_radius_size', 'Preferred Radius Size') }}
                     <br/>
-                    {{ Form::radio('Distance', '1') }} less than 5km
+                    {{ Form::radio('distance', '1') }} less than 5km
                     <br/>
-                    {{ Form::radio('Distance', '2') }} less than 10km
+                    {{ Form::radio('distance', '2') }} less than 10km
                     <br/>
-                    {{ Form::radio('Distance','3') }} Stores that deliver
+                    {{ Form::radio('distance','3') }} Stores that deliver
                     <br/>
                 </div>
 
