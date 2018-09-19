1feeda4860cf13469375cd5f1cf06b9cee07edae
Paul Le <s3539320@student.rmit.edu.au>
2018-09-19 22:50:31 +1000

    Added links in dropdown of navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,8 +62,16 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
+                                
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('dashboard') }}">
+                                    {{ __('Profile') }}
+                                </a>
+
+                                <a class="dropdown-item" href="{{ route('preferences.index') }}">
+                                    {{ __('Preferences') }}
+                                </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -75,6 +83,7 @@
                                     </form>
                                 </div>
                             </li>
+
                         @endguest
                     </ul>
                 </div>
