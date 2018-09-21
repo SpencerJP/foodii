07b9830f19b4ecddbc2c8f806b9a79662fcdeb18
Splerge <damnsplurge@gmail.com>
2018-09-21 18:11:22 +1000

    temporarily changed restaurant_pool_size to 0

--- config/quizoptions.php
+++ config/quizoptions.php
@@ -4,7 +4,7 @@ return [
     'quiz_question_max' => 5,
     'quiz_question_min' => 3,
     // maximum of how many remaining results before we 'true random' a result
-    'restaurant_pool_size' => 15,
+    'restaurant_pool_size' => 0,
     'debug_mode' => true,
 ];
 ?>
