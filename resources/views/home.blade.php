92f5d7a9c6a75c1742b7a25219e94a82e555f9b4
Splerge <damnsplurge@gmail.com>
2018-09-28 19:21:56 +1000

    changed absolute path to relative one

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,8 @@
                 <div class="card-body">
                     <div id="container">
                         <button onclick="location.href= '{{ route('preferences.index') }}'" type="button" id="button1">Preferences</button>
-                        <button onclick="location.href= '{{ url('usershistory') }}'" type="button" id="button2">User's History</button>
-					    <a href="http://127.0.0.1:8000/quiz">
+                        <button onclick="location.href= '{{ route('usershistory') }}'" type="button" id="button2">User's History</button>
+					    <a href="{{ route('quiz.startQuiz') }}">
 				        	<img src="/images/start_button1.png" >
 				    	</a>
                     </div>
