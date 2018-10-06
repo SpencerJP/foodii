60fded7a897e21eba5f0da46b80c73841c00b43f
AndrewChen01 <41568252+AndrewChen01@users.noreply.github.com>
2018-10-06 17:32:42 +1000

    update rating

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,35 +1,100 @@
+
+
 @extends('layouts.app')
 
+<style>
+@import url(//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
+
+fieldset, label { margin: 0; padding: 0; }
+body{ margin: 20px; }
+h1 { font-size: 1.5em; margin: 10px; }
+
+/****** Style Star Rating Widget *****/
+
+.rating { 
+  border: none;
+  float: left;
+}
+
+.rating > input { display: none; } 
+.rating > label:before { 
+  margin: 5px;
+  font-size: 1.25em;
+  font-family: FontAwesome;
+  display: inline-block;
+  content: "\f005";
+}
+
+.rating > .half:before { 
+  content: "\f089";
+  position: absolute;
+}
+
+.rating > label { 
+  color: #ddd; 
+ float: right; 
+}
+
+/***** CSS Magic to Highlight Stars on Hover *****/
+
+.rating > input:checked ~ label, /* show gold star when clicked */
+.rating:not(:checked) > label:hover, /* hover current star */
+.rating:not(:checked) > label:hover ~ label { color: #FFD700;  } /* hover previous stars in list */
+
+.rating > input:checked + label:hover, /* hover current star when changing rating */
+.rating > input:checked ~ label:hover,
+.rating > label:hover ~ input:checked ~ label, /* lighten current selection */
+.rating > input:checked ~ label:hover ~ label { color: #FFED85;  } 
+
+</style>
+
+
+
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="card-body">
-            <h1>History</h1>
-            <table class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <td></td>
-                        <td>Restaurant Name</td>
-                        <td>Address</td>
-                        <td>Phone #</td>
-                        <td>Rating</td>
-                        <td>Quiz Taken</td>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($quizresult as $key => $value)
-                    <tr>
-                        <td>{{ $value->restaurant_image }}</td>
-                        <td>{{ $value->name }}</td>
-                        <td>{{ $value->address }}</td>
-                        <td>{{ $value->phone_number }}</td>
-                        <td>{{ $value->rating }}</td>
-                        <td>{{ $value->created_at }}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+	
+<main role="main">
+
+	<section class="jumbotron text-center">
+	<div class="container">
+	  <h1 class="jumbotron-heading">Welcome Back! </h1>
+	  <p class="lead text-muted">This is your history</p>
+	  <p class="lead text-muted">You can rate the selected restaurants.</p>
+	</div>
+	</section>
+
+	<div class="container">
+	  <div class="row">
+         @foreach($restaurants as $key => $value)
+		<div class="col-md-6 col-sm-6">
+		  <div class="thumbnauk" style="hegiht: 600px">
+			<img class="card-img-top" src="" alt=""><!--?php echo $restaurant['name/image']?-->
+			    <div class="card-body">
+                    <p class="card-text"><td>Name:</td>{{ $value->name }}</p>
+
+                    <p class="card-text"><td>Rating:</td>
+										<fieldset class="rating">
+										<input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+										<input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+										<input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+										<input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+										<input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+										</fieldset>
+										</p>
+                    <div class="address">
+                        <p class="sub">{{ $value->address }}</p>
+                        <p class="sub">{{ $value->phone_number }}</p>
+                    </div>          
+			    </div>
+			</div>  
+		  </div>
+		</div>
+		 @endforeach
+	  </div>
+	</div>
+	
+</main>
+
+
+
 @endsection
