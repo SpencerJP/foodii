4b6c8ca97376aa6eb14a3a1d7b1425967e420f01
AndrewChen01 <41568252+AndrewChen01@users.noreply.github.com>
2018-10-08 01:24:38 +1100

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    <div id="container">
-                        <button onclick="location.href= '{{ route('preferences.index') }}'" type="button" id="button1">Preferences</button>
-                        <button onclick="location.href= '{{ url('usershistory') }}'" type="button" id="button2">User's History</button>
-        					    <a href="{{ route('quiz.startQuiz') }}">
-        				        	<img src="/images/start_button1.png" >
-        				    	</a>
-                    </div>
-                </div>
-            </div>
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4"><a href="{{ route('quiz.startQuiz') }}"><img src="/images/start_button1.png"></a></h1>
+    </div>
+
+    <div class="card-deck mb-3 text-center">
+        <div class="card mb-6 box-shadow">
+          
+          <a href="{{ route('preferences.index') }}"><img src="/images/preference_button.png"></a>
+          
+          
+        </div>
+        <div class="card mb-6 box-shadow">
+          
+          <a href="{{ url('usershistory') }}"><img src="/images/history.png "></a>
         </div>
     </div>
+        
 </div>
 @endsection
+
