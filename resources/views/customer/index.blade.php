b5424a83a67780254c7e1bebdb821f9fd75fb889
s3511842 <s3511842@student.rmit.edu.au>
2018-10-07 15:06:32 +1100

    History Rating Submission
    Rating Submits, Controller still needs to be built to take the rating value and put in database

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -75,16 +75,21 @@ h1 { font-size: 1.5em; margin: 10px; }
                 <p class="sub">{{ $value->address }}</p>
                 <p class="sub">{{ $value->phone_number }}</p>
               </div>
+              <form action="{{ route('history.rate') }}" method="POST">
+              <p class="card-text"><td>Rating:</td>
 
-            <p class="card-text"><td>Rating:</td>
-							<fieldset class="rating">
-								<input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-								<input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-								<input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
-								<input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-								<input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
-							</fieldset>
-						</p>
+                <fieldset class="rating" method="POST">
+  								<input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+  								<input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+  								<input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+  								<input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+  								<input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+  							</fieldset>
+
+                {{ Form::submit('Rate', array('class' => 'btn btn-primary')) }}
+                @csrf
+  						</p>
+          </form>
         </div>
 			 </div>
 		  </div>
