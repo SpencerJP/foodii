e5ce28222fee741479968769c07181b8410dee99
AndrewChen01 <41568252+AndrewChen01@users.noreply.github.com>
2018-10-08 01:28:35 +1100

    question page

--- resources/views/quiz/question.blade.php
+++ resources/views/quiz/question.blade.php
@@ -4,12 +4,20 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="card-body">
-          <h2>{{ $question->questionvalue }}</h2>
+          <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+            <div class="col-md-6 px-0">
+            <h2 class="display-4 font-italic">{{ $question->questionvalue }}</h2>
+            </div>
+            </div>
           @foreach($question->answers as $key => $answer)
             <form action="{{ route('quiz.answerquestion') }}" method="POST">
                 @csrf
-                <input type="hidden" value="{{$answer->id}}" name="answer_id" />
-                <button class="btn btn-small btn-danger">{{$answer->answervalue}}</button>
+            <div class="col-md-12">
+            <div class="card-body d-flex flex-column align-items-start">
+		        <input type="hidden" value="{{$answer->id}}" name="answer_id" />
+                <button class="btn btn-default btn-lg btn-block">{{$answer->answervalue}}</button>
+        	</div>
+            </div>
             </form>
           @endforeach
 
