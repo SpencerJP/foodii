404e91a36a9400d092928873dcf78beca5b896ed
AndrewChen01 <41568252+AndrewChen01@users.noreply.github.com>
2018-10-11 17:19:21 +1100

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,19 +9,18 @@
         </p>   
         <p class="lead mb-0">This is <a href="#" class="text-white font-weight-bold">Foodii</a></p>   
     </div>
-    
-</div>
-<h2 class="blog-post-title">As your server</h2>
-<p>
-Foodii, a free mobile friendly website making it simpler for users to choose what food they are craving, based off the results of our quick quiz. After answering a handful of questions, we recommend a restaurant that fills out the criteria of what the user wants and what they don’t want. If you’re not feeling the restaurant, you can reroll for the next best restaurant.
-</p>
-<hr>
-<p>
-The questions to the user will narrow down the choices of restaurants around their area. These questions would allow our algorithm to match a restaurant to the user.
-</p>
-<hr>
-<p>
-Users can save their preferences, such as dietary requirements and allergies, on their own profile, to make their next experience even smoother.
-</p>   
+    </div>
+    <h2 class="blog-post-title">As your server</h2>
+        <p>
+        Foodii, a free mobile friendly website making it simpler for users to choose what food they are craving, based off the results of our quick quiz. After answering a handful of questions, we recommend a restaurant that fills out the criteria of what the user wants and what they don’t want. If you’re not feeling the restaurant, you can reroll for the next best restaurant.
+        </p>
+        <hr>
+        <p>
+        The questions to the user will narrow down the choices of restaurants around their area. These questions would allow our algorithm to match a restaurant to the user.
+        </p>
+        <hr>
+        <p>
+        Users can save their preferences, such as dietary requirements and allergies, on their own profile, to make their next experience even smoother.
+        </p>   
 </div>
 @endsection
