dd78b29bb5ca7ee0dacde9d60cb4b41488764dbe
AndrewChen01 <41568252+AndrewChen01@users.noreply.github.com>
2018-10-11 17:18:59 +1100

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,35 +2,26 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">About</div>
-
-                <div class="card-body">
-                    <p>
-                    We are committed to providing hungry people a better experience. In today’s fast-paced society, the many food ordering apps, such as Ubereats, Foodora & Menulog, give the public a wide range of choices. However, as a result, people often have difficulty figuring out what they actually feel like eating. We want to simplify this process to make choice easier for the consumer, and to help advertise the many restaurants available in the consumer’s area. We will choose the restaurant for you based off your desires.
-                    </p>
-
-                    <p>                    
-                    This is <b>Foodii</b>.
-                    </p>
-
-                    <p>
-                    Foodii, a free mobile friendly website making it simpler for users to choose what food they are craving, based off the results of our quick quiz. After answering a handful of questions, we recommend a restaurant that fills out the criteria of what the user wants and what they don’t want. If you’re not feeling the restaurant, you can reroll for the next best restaurant.
-                    </p>
-
-                    <p>
-                    The questions to the user will narrow down the choices of restaurants around their area. These questions would allow our algorithm to match a restaurant to the user.
-                    </p>
-
-                    <p>
-                    Users can save their preferences, such as dietary requirements and allergies, on their own profile, to make their next experience even smoother.
-                    </p>    
-                    
-                </div>
-            </div>
-        </div>
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+    <div class="col-md-12 px-0">
+        <h1 class="display-4 font-italic">About</h1>
+        <p class="lead my-3">We are committed to providing hungry people a better experience. In today’s fast-paced society, the many food ordering apps, such as Ubereats, Foodora & Menulog, give the public a wide range of choices. However, as a result, people often have difficulty figuring out what they actually feel like eating. We want to simplify this process to make choice easier for the consumer, and to help advertise the many restaurants available in the consumer’s area. We will choose the restaurant for you based off your desires.
+        </p>   
+        <p class="lead mb-0">This is <a href="#" class="text-white font-weight-bold">Foodii</a></p>   
     </div>
+    
+</div>
+<h2 class="blog-post-title">As your server</h2>
+<p>
+Foodii, a free mobile friendly website making it simpler for users to choose what food they are craving, based off the results of our quick quiz. After answering a handful of questions, we recommend a restaurant that fills out the criteria of what the user wants and what they don’t want. If you’re not feeling the restaurant, you can reroll for the next best restaurant.
+</p>
+<hr>
+<p>
+The questions to the user will narrow down the choices of restaurants around their area. These questions would allow our algorithm to match a restaurant to the user.
+</p>
+<hr>
+<p>
+Users can save their preferences, such as dietary requirements and allergies, on their own profile, to make their next experience even smoother.
+</p>   
 </div>
 @endsection
